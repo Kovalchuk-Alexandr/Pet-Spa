4fad6ee205b6c444529eaeaf6201435d7d6bb403
Kovalchuk-Alexandr <mo250568kaa@gmail.com>
2024-11-22 11:28:17 +0200

    final

---
+++ js/main.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navBtn = document.querySelector(".mobile-nav-btn");
+    // const closeBtn = document.querySelector(".mobile-close-btn");
+    const nav = document.querySelector(".mobile-nav");
+    const menuIcon = document.querySelector(".mobile-nav-btn__icon");
+
+    const fade = document.querySelector(".mobile-nav-fade");
+
+    navBtn.onclick = toggleMobile;
+    // closeBtn.onclick = toggleMobile;
+    fade.onclick = toggleMobile;
+
+    function toggleMobile() {
+        // nav.classList.toggle("mobile-nav");
+        nav.classList.toggle("active");
+        navBtn.classList.toggle("active");
+        menuIcon.classList.toggle("active");
+        document.body.classList.toggle("no-scroll");
+        fade.classList.toggle("mobile-nav-fade--open");
+    }
+});
+
+/* Initialize Swiper */
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    // direction: "vertical",
+    // spaceBetween: 30,
+    // loop: true,
+    slidesPerView: 1,
+    // freeMode: true,
+    grabCursor: true,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: ".swiper-scrollbar",
+    // },
+});
+
+/* Пример замыкания */
+function startCounter() {
+    let counter = 0;
+
+    return {
+        increase: function () {
+            ++counter;
+        },
+        getCounter: function () {
+            return counter;
+        },
+    };
+}
+
+const counter = startCounter();
+console.log(counter.getCounter());
+counter.increase();
+counter.increase();
+counter.increase();
+console.log(counter.getCounter());
